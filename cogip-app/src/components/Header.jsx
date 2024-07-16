3f39f95dd402e21c0246b1ebd7319188594ac5bf
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-07-16 09:44:00 +0200

    update styling header and footer

--- cogip-app/src/components/Header.jsx
+++ cogip-app/src/components/Header.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 
 function Header() {
     return (
-      <>
-        <div>
-            <h1>COGIP</h1>
-            <nav>
-                <a href="">Home</a>
-                <a href="">Invoices</a>
-                <a href="">Campanies</a>
-                <a href="">Contacts</a>
-                <a href=""></a>
-            </nav>
-        </div>
-        <div>
+      <div className='border-[30px] border-yellow-300 flex justify-between bg-yellow-300 items-center'>
+        <h1 className='text-4xl'>COGIP</h1>
+        <nav className='flex space-x-14' >
+            <a href="">Home</a>
+            <a href="">Invoices</a>
+            <a href="">Campanies</a>
+            <a href="">Contacts</a>
+            <a href=""></a>
+        </nav>
+        <div  className='flex space-x-14 mr-14'>
             <a href="">Sign up</a>
             <a href="">Login</a>
         </div>
-      </>
+        
+      </div>
     )
   }
   export default Header
