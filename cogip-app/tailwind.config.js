d8440b36f03f7a614d5cbef143116525f578e731
lynekpng <lynekpng@gmail.com>
2024-07-17 09:22:53 +0200

    update footer / header

--- cogip-app/tailwind.config.js
+++ cogip-app/tailwind.config.js
@@ -1,24 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "cogip-color": "#F9DE4E",
       },
+      fontFamily: {
+        'Inter': ["Inter", "sans-serif"],
+        'Assisant': ["Assistant", "sans-serif"],
+        'Roboto': ["Roboto", "Sans-serif"],
+      },
     },
   },
   plugins: [],
 };
-=======
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> 3f39f95dd402e21c0246b1ebd7319188594ac5bf
