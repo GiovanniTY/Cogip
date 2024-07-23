cf1d4b9d956f7b177fe85e07bdebe30264b3f828
isab95 <noelisabelle15@gmail.com>
2024-07-23 12:03:41 +0200

    change selectAll of contact

--- Controllers/ContactsController.php
+++ Controllers/ContactsController.php
@@ -16,7 +16,7 @@ class ContactsController {
 
     // Function to select all datas of contacts in the DB
     public function getAllContacts(){
-        $query = "SELECT * FROM contacts";
+        $query = "SELECT contacts.id, contacts.name, contacts.company_id, contacts.email, contacts.phone, DATE_FORMAT(contacts.created_at, '%d/%m/%Y') as created_at, DATE_FORMAT(contacts.updated_at, '%d/%m/%Y') as updated_at, companies.name as company FROM contacts LEFT JOIN companies ON contacts.company_id = companies.id ORDER BY contacts.created_at DESC";
         $stmt = $this->db->query($query);
         $contactsData = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
@@ -89,6 +89,7 @@ class ContactsController {
             $contactData['id'],
             $contactData['name'],
             $contactData['company_id'],
+            $contactData['company'],
             $contactData['email'],
             $contactData['phone'], 
             $contactData['created_at'],
