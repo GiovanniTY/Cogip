2458b8d9b9dd877b73ca8a2ca0329121a18b5031
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-07-25 13:29:17 +0200

    responsive updated

--- cogip-app/src/components/dashboard/DashboardLayout.jsx
+++ cogip-app/src/components/dashboard/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import dashboardImage from '../../assets/Layer 2.png';
+import menu from "../../assets/hamburger_icon.png";
 
 const DashboardLayout = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -28,24 +29,20 @@ const DashboardLayout = ({ children }) => {
   return (
     <div className="flex h-screen relative">
       <Sidebar isSidebarOpen={isSidebarOpen} closeSidebar={closeSidebar} />
-      <div className="flex-1 flex flex-col">
-        <header className="p-4 md:p-6 bg-white shadow-md flex items-center justify-between z-40 relative">
+      <div className="md:pl-6 bg-gray-100 flex-1 flex flex-col">
+        <header className="p-4 md:p-6 flex items-center justify-between z-40 relative">
           <button onClick={toggleSidebar} className="text-2xl md:hidden">
-            <div className="w-8 h-8 flex flex-col justify-between items-center">
-              <span className="block w-6 h-1 bg-gray-700"></span>
-              <span className="block w-6 h-1 bg-gray-700"></span>
-              <span className="block w-6 h-1 bg-gray-700"></span>
-            </div>
+            <img className="w-8 h-8 flex flex-col justify-between items-center" src={menu} alt="hamburger-icon" />
           </button>
-          <h1 className='text-2xl md:text-3xl font-bold'>Dashboard</h1>
+          <h1 className='text-2xl md:text-4xl font-black'>Dashboard</h1>
         </header>
-        <div className='relative flex flex-col md:flex-row items-center p-4 bg-indigo-400 text-white rounded md:space-x-4'>
-          <p className='text-lg md:text-xl font-bold'>
-            Welcome back Henry!<br />You can here add an invoice, a company, and some contacts
+        <div className='relative flex flex-col md:flex-row items-center p-4 bg-[#9698D6] text-white rounded md:space-x-4'>
+          <p className='text-lg md:text-xl font-light'>
+            <span className='text-3xl font-bold'>Welcome back Henry!</span><br />You can here add an invoice, a company and some contacts
           </p>
-          <img className='absolute top-0 right-0 md:relative md:top-auto md:right-auto md:w-1/3 lg:w-1/4' src={dashboardImage} alt="Dashboard"/>
+          <img className='absolute md:relative md:w-1/3 lg:w-1/4' src={dashboardImage} alt="Dashboard"/>
         </div>
-        <main className="flex-1 p-4 md:p-6 overflow-y-auto bg-gray-100">
+        <main className="flex-1 p-4 md:p-6 overflow-y-auto">
           {children}
         </main>
       </div>
